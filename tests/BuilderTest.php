ecd1134c5f38376c32be018e37b74e2542d6feb6
dan <danmcadams@gmail.com>
2017-12-23 21:07:24 -0600

    testing scrutinizer

---
+++ tests/BuilderTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace CsvMachine\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class BuilderTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertFalse(false);
+    }
+}
