31784e0881da7ab7a87f2bb0c917afeba3ee3730
Dan McAdams <danmcadams@gmail.com>
2018-03-29 12:04:21 -0500

    add camel case splitter formatter

---
+++ src/Csv/Formatter/SplitCamelCaseWordsFormatter.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the Csv-Machine package.
+ *
+ * (c) Dan McAdams <danmcadams@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RoadBunch\Csv\Formatter;
+
+
+class SplitCamelCaseWordsFormatter implements FormatterInterface
+{
+    /** @var FormatterInterface */
+    protected $formatter;
+
+    public function __construct()
+    {
+        // @todo Make sure to uppercase the first word of a string like userFirstName -> User First Name
+        $callback = function ($var) {
+            $regEx  = '/(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])/x';
+            $words  = preg_split($regEx, $var);
+            return implode(' ', $words);
+        };
+        $this->formatter = new Formatter($callback);
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return array
+     */
+    public function format(array $data): array
+    {
+        return $this->formatter->format($data);
+    }
+}
---
+++ tests/Formatter/SplitCamelCaseWordsFormatterTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the Csv-Machine package.
+ *
+ * (c) Dan McAdams <danmcadams@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RoadBunch\Csv\Tests\Formatter;
+
+
+use PHPUnit\Framework\TestCase;
+use RoadBunch\Csv\Formatter\SplitCamelCaseWordsFormatter;
+
+/**
+ * Class SplitCamelCaseWordsFormatterTest
+ *
+ * @author  Dan McAdams
+ * @package RoadBunch\Csv\Tests\Formatter
+ */
+class SplitCamelCaseWordsFormatterTest extends TestCase
+{
+    public function testCreateFormatter()
+    {
+        $ccWordFormatter = new SplitCamelCaseWordsFormatter();
+        $this->assertNotNull($ccWordFormatter);
+    }
+
+    public function testSplitCamelCase()
+    {
+        $ccHeader = ['TestString', 'TheseShouldSeparate', 'AndCSVShouldWork'];
+        $expected = ['Test String', 'These Should Separate', 'And CSV Should Work'];
+
+        $formatter = new SplitCamelCaseWordsFormatter();
+        $formattedHeader = $formatter->format($ccHeader);
+
+        $this->assertEquals($expected, $formattedHeader);
+    }
+}
