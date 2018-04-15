066045926a90e5e4a384ed445652b1ac2179a8b6
Dan McAdams <danmcadams@gmail.com>
2018-04-14 19:38:39 -0500

    add addRows method, missed this somehow

--- src/Csv/WriterInterface.php
+++ src/Csv/WriterInterface.php
@@ -31,6 +31,12 @@ interface WriterInterface
     public function addRow(array $row): WriterInterface;
 
     /**
+     * @param array $rows
+     * @return mixed
+     */
+    public function addRows(array $rows);
+
+    /**
      * Write the CSV to the stream
      * @param string $filename
      */
